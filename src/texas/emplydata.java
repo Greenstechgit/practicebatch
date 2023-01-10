e6bc167e5a85a99261df6088509d133271dd099c
amutha20 <amuthabalaji6@gmail.com>
2023-01-10 10:57:48 +0530

    new code added

---
+++ src/texas/emplydata.java
@@ -0,0 +1,15 @@
+package texas;
+
+public class emplydata {
+	private void username() {
+		System.out.println("enter the username");
+	}
+private void password() {
+	System.out.println("enter the password");
+}
+public static void main(String[] args) {
+	emplydata a=new emplydata();
+	a.username();
+	a.password();
+}
+}
